c0f81838733d488418a35880d434f79574f128a8
David Winer <davidjwiner@davids-mbp.home>
2015-05-06 09:07:49 -0400

    Adding article validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,8 +14,10 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
 
-		@article.save
-		redirect_to @article
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
 	end
 
 	private
